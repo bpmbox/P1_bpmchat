d3d2a7855ba6294074b7f96230715e62803333cf
kenichi miyata <miyataken999@bpmboxes.com>
2021-12-13 01:07:06 +0900

    Update test.js

--- test.js
+++ test.js
@@ -29,4 +29,216 @@ const View = ({ children }: { children: Node }) => {
     </div>
   );
 }
+    
+
+
+    class Greeting extends React.Component {
+  render() {
+    return <h1>Hi!</h1>;
+  }
+}
+
+    class VoteButton2 extends React.Component {
+      constructor(props) {
+        super(props);
+        this.state = {
+          voted: false,
+          name: "",
+          text: "",
+        };
+        this.onChangeName = this.onChangeName.bind(this);
+        this.onChangeText = this.onChangeText.bind(this);
+
+      }
+
+      onChangeText(e) {
+        this.setState({
+          text: e.target.value,
+        });
+      }
+
+
+      onChangeTitle(e) {
+        this.setState({
+          title: e.target.value,
+        });
+      }
+
+      onChangeName(e) {
+        this.setState({
+          name: e.target.value,
+        });
+      }
+
+      saveTutorial() {
+      alert("test")
+        let data = {
+          title: this.state.title,
+          description: this.state.description,
+          published: false,
+          name: this.state.name,
+          text: this.state.text,
+        };
+        /*
+            TutorialDataService.create(data)
+              .then(() => {
+                console.log("Created new item successfully!");
+                this.setState({
+                  submitted: true,
+                });
+              })
+              .catch((e) => {
+                console.log(e);
+              });*/
+      }
+
+      render() {
+        if (this.state.voted) {
+          return (
+            <div>
+              <div className="form-group">
+                <label htmlFor="description">text</label>
+                <textarea
+                  type="text"
+                  className="form-control"
+                  id="text"
+                  required
+                  value={this.state.text}
+                  onChange={this.onChangeText}
+                  name="text"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="description">nme</label>
+                <textarea
+                  type="text"
+                  className="form-control"
+                  id="name"
+                  required
+                  value={this.state.name}
+                  onChange={this.onChangeName}
+                  name="name"
+                />
+              </div>
+              <button onClick={this.saveTutorial()} className="btn btn-success">
+                Submit
+            </button>
+              <button onClick={
+                () => this.setState({ voted: false })
+              }>
+                voted
+                        </button>
+            </div>
+          )
+        } else {
+          return (
+            <button onClick={() => this.setState({ voted: true })}>
+              vote
+            </button>
+          );
+        }
+      }
+    }
+
+    class VoteButton extends React.Component {
+      constructor(props) {
+        super(props);
+        this.state = {
+          voted: false,
+          name: "",
+          text: "",
+        };
+        this.onChangeName = this.onChangeName.bind(this);
+        this.onChangeText = this.onChangeText.bind(this);
+
+      }
+
+      onChangeText(e) {
+        this.setState({
+          text: e.target.value,
+        });
+      }
+
+      onChangeTitle(e) {
+        this.setState({
+          title: e.target.value,
+        });
+      }
+
+      onChangeName(e) {
+        this.setState({
+          name: e.target.value,
+        });
+      }
+
+      saveTutorial() {
+      alert("compornent test")
+        let data = {
+          title: this.state.title,
+          description: this.state.description,
+          published: false,
+          name: this.state.name,
+          text: this.state.text,
+        };
+        /*
+            TutorialDataService.create(data)
+              .then(() => {
+                console.log("Created new item successfully!");
+                this.setState({
+                  submitted: true,
+                });
+              })
+              .catch((e) => {
+                console.log(e);
+              });*/
+      }
+
+      render() {
+        if (this.state.voted) {
+          return (
+            <div>
+              <div className="form-group">
+                <label htmlFor="description">text</label>
+                <textarea
+                  type="text"
+                  className="form-control"
+                  id="text"
+                  required
+                  value={this.state.text}
+                  onChange={this.onChangeText}
+                  name="text"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="description">name</label>
+                <textarea
+                  type="text"
+                  className="form-control"
+                  id="name"
+                  required
+                  value={this.state.name}
+                  onChange={this.onChangeName}
+                  name="name"
+                />
+              </div>
+              <button onClick={this.saveTutorial} className="btn btn-success">
+                Submit
+            </button>
+              <button onClick={
+                () => this.setState({ voted: false })
+              }>
+                voted
+                        </button>
+            </div>
+          )
+        } else {
+          return (
+            <button onClick={() => this.setState({ voted: true })}>
+              vote
+            </button>
+          );
+        }
+      }
+    }
+
 
