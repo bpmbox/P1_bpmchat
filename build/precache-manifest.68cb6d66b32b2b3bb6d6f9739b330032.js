8873f964bba6dfd6b002184272360e357ac2769c
kenichi miyata <miyataken999@bpmboxes.com>
2021-12-12 18:57:41 +0000

    update

---
+++ build/precache-manifest.68cb6d66b32b2b3bb6d6f9739b330032.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "208188af6a15d9f6095d107bb656e557",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1453ca3cdbdd04ff9577",
+    "url": "/static/css/2.e998d647.chunk.css"
+  },
+  {
+    "revision": "b093c30f811c931ff2f8",
+    "url": "/static/css/main.5b148433.chunk.css"
+  },
+  {
+    "revision": "1453ca3cdbdd04ff9577",
+    "url": "/static/js/2.a763f9cc.chunk.js"
+  },
+  {
+    "revision": "731f815fa135396b018bfdb203439618",
+    "url": "/static/js/2.a763f9cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b093c30f811c931ff2f8",
+    "url": "/static/js/main.2b22b464.chunk.js"
+  },
+  {
+    "revision": "55d488e9e81e555a03c4",
+    "url": "/static/js/runtime-main.077a2d11.js"
+  }
+]);
