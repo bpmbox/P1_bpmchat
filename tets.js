62201fb687d38843dd7758c8d0efa8c545835494
kenichi miyata <miyataken999@bpmboxes.com>
2021-12-12 19:01:50 +0900

    Update tets.js

--- tets.js
+++ tets.js
@@ -1,8 +1,23 @@
-class test{
-gastest(test){
- Logger.log(test) 
-}
-}
+function test(){
 
-return new test()
+  // Personクラスを定義する（引数付き）
+  class Person {
+    // プロパティを追加する
+    constructor(name,greetingword) {
+      this.name = name;
+      this.greetingword = greetingword;
+    }
+    
+    // メソッドを追加する
+　　greeting(){
+        Browser.msgBox(this.greetingword);
+    }
+  }
+
+　// new演算子で引数を渡してインスタンスを作成する
+　var hirachin = new Person("ひらちん","オッス！")
+
+　// メソッドを実行する
+　hirachin.greeting()
+}
 
