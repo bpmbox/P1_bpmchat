9a7d166f3de62f2bfd2448c9919805b494fad32e
kenichi miyata <miyataken999@bpmboxes.com>
2021-12-15 00:43:30 +0900

    ddd

--- tets.js
+++ tets.js
@@ -1,44 +1,44 @@
 
 //GASプラグイン言葉によって処理を変更します
-function test(val){
-  
-  function didmount(){
-  }
-  
-  function willmount(){
+function test(val) {
+
+  function didmount() {
   }
 
-function myFunction22_02_01() {
-  const response = UrlFetchApp.fetch('https://tonari-it.com');
-  console.log(response.getResponseCode()); //200
-  
-  const headers = response.getHeaders();
-  for (const key in headers) console.log(key, headers[key]);
+  function willmount() {
+  }
+  //harahetta       sssssssssssssssss
+  function myFunction22_02_01() {
+    const response = UrlFetchApp.fetch('https://tonari-it.com');
+    console.log(response.getResponseCode()); //200
 
-  console.log(response.getContentText());
-}
+    const headers = response.getHeaders();
+    for (const key in headers) console.log(key, headers[key]);
+
+    console.log(response.getContentText());
+  }
 
 
   // Personクラスを定義する（引数付き）
   class Person {
     // プロパティを追加する
-    constructor(name,greetingword) {
+    constructor(name, greetingword) {
       this.name = name;
       this.greetingword = greetingword;
     }
-    
+
     // メソッドを追加する
-　　greeting(){
-        Logger.log(this.greetingword+val);
-    return this.greetingword
+    greeting() {
+      Logger.log(this.greetingword + val);
+      return this.greetingword
     }
   }
 
-　// new演算子で引数を渡してインスタンスを作成する
-  if(val == "日本語"){
-　   var hirachin = new Person("ひらちん","オッス！")
-　   // メソッドを実行する
-　   return hirachin.greeting()
+  // new演算子で引数を渡してインスタンスを作成する
+  if (val == "日本語") {
+    var hirachin = new Person("ひらちん", "オッス！")
+    // メソッドを実行する
+    return hirachin.greeting()
   }
 }
 
