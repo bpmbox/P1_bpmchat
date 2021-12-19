590c426510a15c23ca2fc65dde2f27ed499cbf45
kenichi miyata <miyataken999@bpmboxes.com>
2021-12-20 03:37:13 +0900

    Update test.js

--- test.js
+++ test.js
@@ -52,8 +52,30 @@ class VoteButton2 extends React.Component {
     };
     this.onChangeName = this.onChangeName.bind(this);
     this.onChangeText = this.onChangeText.bind(this);
+    
+        var config = {
+      apiKey: "AIzaSyCsOjFyAAuFr1CITcnufG-GpZBKpLgUP90",
+      authDomain: "rpa999-56929.firebaseapp.com",
+      databaseURL: "https://rpa999-56929.firebaseio.com",
+      projectId: "rpa999-56929",
+      storageBucket: "rpa999-56929.appspot.com",
+      messagingSenderId: "155298248089",
+      appId: "1:155298248089:web:e25d64dc9f5370c4"
+    };
+
+    firebase.initializeApp(config);
+
+    // データベースの参照を準備
+    this.messagesRef = firebase.database().ref().child('<?=ken?>')
+    this.messagesRefs = firebase.database().ref().child('<?=ken?>/search')
 
   }
+  
+      //dataが変更の場合
+    this.messagesRef.on('child_changed', function (data) {
+      alert("data changed")
+ 
+    });
 
   onChangeText(e) {
     this.setState({
